1b37fb97dc3d60482511ab87649ab600490bb03a
randyluna <135148119+randyluna@users.noreply.github.com>
2023-07-24 16:43:48 -0400

    changes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+
+const messages = [
+  "Learn React ⚛️",
+  "Apply for jobs 💼",
+  "Invest your new income 🤑",
+];
 
 function App() {
+  const [step, setStep] = useState(1);
+  const [isOpen, setIsOpen] = useState(true);
+  // console.log(arr)
+
+  // let step = 1;
+
+  function handdlePrevious() {
+    if (step > 1) setStep(step - 1);
+  }
+
+  function handleNext() {
+    if (step < 3) setStep(step + 1);
+    // alert('next')
+  }
+
+  function handleClose() {
+    setIsOpen(false)
+  }
+
+  function handleOpen(){
+    setIsOpen(true)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {isOpen ? (
+        
+        <div>
+          <button className="close" onClick={handleClose}>&times;</button>
+              <div className="steps">
+          <div className="numbers">
+            <div className={`${step >= 1 ? "active" : ""}`}>1</div>
+            <div className={`${step >= 2 ? "active" : ""}`}>2</div>
+            <div className={`${step >= 3 ? "active" : ""}`}>3</div>
+          </div>
+          <p className="message">
+            Step {step}: {messages[step - 1]}
+          </p>
+          <div className="buttons">
+            <button
+              style={{ backgroundColor: "#7950f2" }}
+              onClick={handdlePrevious}
+            >
+              Previous
+            </button>
+            <button style={{ backgroundColor: "#7950f2" }} onClick={handleNext}>
+              Next
+            </button>
+          </div>
+        </div>
+        </div>
+      ) : (
+        <div>
+          <h1>Its not here</h1>
+        <button className="close" onClick={handleOpen}> &#10003;</button>
+        </div>
+      )}
     </div>
   );
 }
